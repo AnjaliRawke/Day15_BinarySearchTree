542d1af879651b813a1fe31516bd99b8794a8605
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-29 19:24:02 +0530

    Added: UC1 Ability to create binary search tree

--- src/com/bridgelabz/BinarySearchMain.java
+++ src/com/bridgelabz/BinarySearchMain.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 public class BinarySearchMain {
 	public static void main(String[] args) {
 		BinarySearch<Integer> bst = new BinarySearch<>();
-
 		bst.add(56);
 		bst.add(30);
 		bst.add(70);
